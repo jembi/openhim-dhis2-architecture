d5e93cd10302b5156d4e6bd972410b1ca5325274
MattyJ007 <matthew.dickie@jembi.org>
2020-06-09 14:18:48 +0200

    Update DHIS2 metadata import-export to use Axios
    The node module Axios was added to the instantohie-config-importer
    docker image. This makes the requests to DHIS2 easier as we can switch
    between HTTP and HTTPS without switching modules.
    
    MENC-21

--- docker/import-export/dhis2/volume/metadata.js
+++ docker/import-export/dhis2/volume/metadata.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const axios = require('axios')
 const fs = require('fs')
-const http = require('http')
 const path = require('path')
 const zlib = require('zlib')
+const { PassThrough } = require('stream')
 
+const DHIS2_PROTOCOL = process.env.DHIS2_PROTOCOL || 'http'
 const DHIS2_API_HOSTNAME = process.env.DHIS2_API_HOSTNAME || 'dhis-web'
 const DHIS2_API_PASSWORD = process.env.DHIS2_API_PASSWORD || 'district'
 const DHIS2_API_PORT = process.env.DHIS2_API_PORT || 8080
@@ -39,74 +41,63 @@ exports.importMetaData = async () => {
   )
 
   const options = {
-    protocol: 'http:',
-    hostname: DHIS2_API_HOSTNAME,
-    port: DHIS2_API_PORT,
-    path: '/api/metadata',
-    method: 'POST',
+    url: `${DHIS2_PROTOCOL}://${DHIS2_API_HOSTNAME}:${DHIS2_API_PORT}/api/metadata`,
+    method: 'post',
     headers: {
       'Content-Type': 'application/json',
       'Content-Length': Buffer.byteLength(configData),
       Authorization: `Basic ${authHeader}`
-    }
+    },
+    data: configData
   }
 
-  const req = http.request(options, (res) => {
-    if (res.statusCode == 401) {
-      throw new Error(`Incorrect DHIS2 API credentials`)
-    }
+  try {
+    const response = await axios(options)
 
-    if (res.statusCode != 200) {
-      throw new Error(`Failed to import DHIS2 config: ${res.statusCode}`)
-    }
-
-    console.log('Successfully Imported DHIS2 Config')
-  })
-
-  req.on('error', (error) => {
+    console.log(
+      `Successfully Imported DHIS2 Config.\n\nImport summary:${JSON.stringify(
+        response.data.stats
+      )}`
+    )
+  } catch (error) {
     throw new Error(`Failed to import DHIS2 config: ${error.message}`)
-  })
-
-  req.write(configData)
-  req.end()
+  }
 }
 
 exports.exportMetaData = async () => {
   const options = {
-    protocol: 'http:',
-    hostname: DHIS2_API_HOSTNAME,
-    port: DHIS2_API_PORT,
-    path: '/api/metadata.json',
-    method: 'GET',
+    url: `${DHIS2_PROTOCOL}://${DHIS2_API_HOSTNAME}:${DHIS2_API_PORT}/api/metadata.json`,
+    method: 'get',
     headers: {
       Accept: 'application/json',
       Authorization: `Basic ${authHeader}`
+    },
+    validateStatus: (status) => {
+      return status == 200
     }
   }
 
-  const req = http.request(options, (res) => {
+  try {
+    const response = await axios(options)
+
+    // The gzip module requires a stream of data
+    const streamResponseData = new PassThrough()
+    streamResponseData.write(JSON.stringify(response.data))
+    streamResponseData.end()
+
     const writeStream = fs.createWriteStream(DHIS2_METADATA_FILENAME)
     const zip = zlib.createGzip()
-    if (res.statusCode == 401) {
-      throw new Error(`Incorrect DHIS2 API credentials`)
-    }
 
-    if (res.statusCode != 200) {
-      throw new Error(`Failed to export DHIS2 config: ${res.statusCode}`)
-    }
-
-    res
+    streamResponseData
       .pipe(zip)
       .pipe(writeStream)
       .on('finish', (err) => {
         if (err) throw err
-        console.log('Successfully exported DHIS2 config')
+        console.log(
+          'Successfully exported DHIS2 config.\n\nData saved to metadata.json.gz file.'
+        )
       })
-  })
-
-  req.on('error', (error) => {
+  } catch (error) {
     console.error('Failed to export DHIS2 config: ', error.message)
-  })
-
-  req.end()
+  }
 }
