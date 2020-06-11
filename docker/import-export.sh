5711efe47b48bee26a4dfcdaec118a8b8121a4fd
Martin Brocker <martin.brocker@jembi.org>
2020-06-11 08:29:15 +0200

    Updated import/export bash script
    To include the other services that are able to import/export metadat configurations
    
    MENC-22

--- docker/import-export.sh
+++ docker/import-export.sh
@@ -3,9 +3,12 @@
 COMMAND=$1
 TARGET=$2
 
-if ! [[ "$TARGET" =~ ^(dhis2|openhim|validator|file-queue|all)$ ]]; then
+VALID_OPTIONS="dhis2|openhim|validator-orchestrator|populator|file-queue|facility-cache|all"
+
+if ! [[ "$TARGET" =~ ^($VALID_OPTIONS)$ ]]; then
     TARGET='all'
     echo "Defaulting to 'all' as a target was not specified or invalid"
+    echo "Target options: $VALID_OPTIONS"
 fi
 
 composeFilePath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
@@ -14,12 +17,50 @@ if [ "$COMMAND" == "import" ]; then
     if [[ "$TARGET" =~ ^(dhis2|all)$ ]]; then
         docker-compose -f "$composeFilePath"/import-export/dhis2/docker-compose-importer-dhis2.yml up
     fi
+
+    if [[ "$TARGET" =~ ^(openhim|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/openhim/docker-compose.importer.openhim.yml up
+    fi
+
+    if [[ "$TARGET" =~ ^(validator-orchestrator|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/validator-orchestrator/docker-compose.importer.validator-orchestrator.yml up
+    fi
+
+    if [[ "$TARGET" =~ ^(populator|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/populator/docker-compose.importer.populator.yml up
+    fi
+
+    if [[ "$TARGET" =~ ^(file-queue|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/file-queue/docker-compose.importer.file-queue.yml up
+    fi
+
+    if [[ "$TARGET" =~ ^(facility-cache|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/facility-cache/docker-compose.importer.facility-cache.yml up
+    fi
 elif [ "$COMMAND" == "export" ]; then
-    if ! [[ "$TARGET" =~ ^(dhis2|all)$ ]]; then
-        echo "Export for '$TARGET' no supported"
-    else
+    if [[ "$TARGET" =~ ^(dhis2|all)$ ]]; then
         docker-compose -f "$composeFilePath"/import-export/dhis2/docker-compose-exporter-dhis2.yml up
     fi
+
+    if [[ "$TARGET" =~ ^(openhim|all)$ ]]; then
+        docker-compose -f "$composeFilePath"/import-export/openhim/docker-compose.exporter.openhim.yml up
+    fi
+
+    if [[ "$TARGET" =~ ^(validator-orchestrator|all)$ ]]; then
+        echo "No export option yet for the Validator Orchestrator Mediator"
+    fi
+
+    if [[ "$TARGET" =~ ^(populator|all)$ ]]; then
+        echo "No export option yet for the Populator Mediator"
+    fi
+
+    if [[ "$TARGET" =~ ^(file-queue|all)$ ]]; then
+        echo "No export option yet for the File Queue Mediator"
+    fi
+
+    if [[ "$TARGET" =~ ^(facility-cache|all)$ ]]; then
+        echo "No export option yet for the Faciliy Cache Mediator"
+    fi
 else
-    echo "Valid options are: import or export"
+    echo "Valid command options are: import or export"
 fi
